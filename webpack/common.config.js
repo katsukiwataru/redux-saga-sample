5655d8065b3c5084fd72795735acd2b224236eb8
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-04-05 19:53:30 +0900

    feat: add environment

---
+++ webpack/common.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const createStyledComponentsTransformer = require('typescript-plugin-styled-components').default;
+const styledComponentsTransformer = createStyledComponentsTransformer();
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const root = path.resolve(__dirname, '../');
+
+module.exports = merge({
+  entry: path.resolve(root, 'src/index.tsx'),
+  output: {
+    path: path.resolve(root, 'dist'),
+    filename: 'js/[name].bundle.js',
+    chunkFilename: 'js/[name].[hash].bundle.js',
+    publicPath: '/',
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/](react|react-dom|react-router|react-router-dom)[\\/]/,
+          name: 'vendor',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(root, 'src/index.html'),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              getCustomTransformers: () => ({ before: [styledComponentsTransformer] }),
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+});
