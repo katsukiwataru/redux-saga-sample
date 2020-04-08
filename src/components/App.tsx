627d922ddb48b4b55411f596cdf7bad0309c2f7e
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-04-08 23:40:15 +0900

    feat: add ErrorGenerator

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Counter } from './Counter';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store';
+import { ErrorGenerator } from './ErrorGenerator';
 
 const App: React.FC = () => {
   const count = useSelector<RootState, number>((state) => state.counter.number);
@@ -9,6 +10,7 @@ const App: React.FC = () => {
     <div>
       <p>click {count}</p>
       <Counter />
+      <ErrorGenerator />
     </div>
   );
 };
