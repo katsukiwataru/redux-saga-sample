f3c4587edc6db5a8fd4a3c4422c70032b1589449
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-04-09 00:25:05 +0900

    feat: remove payload

--- src/actions.ts
+++ src/actions.ts
@@ -2,22 +2,18 @@ import { Action } from 'redux';
 
 interface Increment extends Action {
   type: 'INCREMENT';
-  payload: number;
 }
 
 interface IncrementIfOdd extends Action {
   type: 'INCREMENT_IF_ODD';
-  payload: number;
 }
 
 interface Decrement extends Action {
   type: 'DECREMENT';
-  payload: number;
 }
 
 interface ErrorInPut extends Action {
   type: 'ACTION_ERROR_IN_PUT';
-  payload: number;
 }
 
 export type Actions = Increment | IncrementIfOdd | Decrement | ErrorInPut;
