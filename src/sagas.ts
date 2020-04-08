712e1067671995bb797702c1a8f140331644988a
Wataru Katsuki <a17dc050@dhw.ac.jp>
2020-04-09 00:22:01 +0900

    feat: add errorInPutSaga

--- src/sagas.ts
+++ src/sagas.ts
@@ -1,16 +1,25 @@
-import { put, takeEvery, all, call } from 'redux-saga/effects';
+import { put, takeEvery, all, call, delay } from 'redux-saga/effects';
 
-const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+const delayFn = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
 function* incrementAsync() {
-  yield call(delay, 1000);
+  yield call(delayFn, 1000);
   yield put({ type: 'INCREMENT' });
 }
 
-export function* watchIncrementAsync() {
+export function* errorInPutSaga() {
+  yield delay(100);
+  yield put({ type: 'ACTION_ERROR_IN_PUT' });
+}
+
+function* watchIncrementAsync() {
   yield takeEvery('INCREMENT_ASYNC', incrementAsync);
 }
 
+function* watchErrorInPutSaga() {
+  yield takeEvery('ACTION_ERROR_IN_PUT', errorInPutSaga);
+}
+
 export function* rootSaga() {
-  yield all([watchIncrementAsync()]);
+  yield all([watchIncrementAsync(), watchErrorInPutSaga()]);
 }
